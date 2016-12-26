908750094c1e5160e1d7f4edcdbe9484064d1160
davandev <davan_at_work@hotmail.com>
2016-12-27 00:01:35 +0100

    Update with some debug

--- davan/http/service/monitor/ActiveScenesMonitorService.py
+++ davan/http/service/monitor/ActiveScenesMonitorService.py
@@ -57,7 +57,7 @@ class ActiveScenesMonitorService(BaseService):
         try:
             for scene in self.config['MONITOR_SCENES']:
                 scene_url = self.config['GET_STATE_SCENE_URL'].replace("<ID>",scene)
-                self.logger.info("Check state of " + scene_url)
+                self.logger.info("Check state of " + scene)
 
                 result = urllib.urlopen(scene_url)
                 res = result.read()
